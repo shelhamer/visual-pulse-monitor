225dc0fcd4ce40475d410ab67d226377571fe959
Evan Shelhamer <shelhamer@imaginarynumber.net>
2011-12-06 11:35:50 -0500

    Fix positive frequency domain selection: exclude 0

--- code/analyse_power_spectrum.m
+++ code/analyse_power_spectrum.m
@@ -14,9 +14,9 @@ function [pows, freq] = analyse_power_spectrum(X, Fs)
   freq = k/T;
 
   % select the positive domain FFT and range
-  zero_idx = fN/2 + 1;
-  amp = amp(zero_idx:end);
-  freq = freq(zero_idx:end);
+  one_idx = fN/2 + 2;
+  amp = amp(one_idx:end);
+  freq = freq(one_idx:end);
 
   % return power spectrum
   pows = abs(amp).^2;
