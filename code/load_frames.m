dd9fb0f443d1420179f46e1b17fa5a1811e70130
Evan Shelhamer <shelhamer@imaginarynumber.net>
2011-12-05 19:01:16 -0500

    Add metadeta output for video loading: dimensions & fps

--- code/load_frames.m
+++ code/load_frames.m
@@ -1,5 +1,6 @@
 % load frames from video file (simple convenience function)
 function [frames] = load_frames(filename)
   vid = VideoReader(filename);
+  fprintf('loading frames from %dx%d video recorded at %f fps\n', [vid.Width vid.Height vid.FrameRate]);
   frames = read(vid);
 end
