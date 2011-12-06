e4bc5cbc71f64a599ebab26b988dffa0c187ce6a
Evan Shelhamer <shelhamer@imaginarynumber.net>
2011-12-05 19:16:53 -0500

    Allow partial frame loading through frame range arg (save memory)

--- code/load_frames.m
+++ code/load_frames.m
@@ -1,6 +1,14 @@
 % load frames from video file (simple convenience function)
-function [frames] = load_frames(filename)
+function [frames] = load_frames(filename, frame_range)
   vid = VideoReader(filename);
   fprintf('loading frames from %dx%d video recorded at %f fps\n', [vid.Width vid.Height vid.FrameRate]);
-  frames = read(vid);
+  if nargin == 1
+    frames = read(vid);
+  else
+    num_frames = frame_range(2)-frame_range(1)+1;
+    frames = zeros([vid.Height vid.Width 3 num_frames], 'uint8');
+    for idx=1:num_frames
+      frames(:,:,:,idx) = read(vid, idx-1+frame_range(1));
+    end
+  end
 end
